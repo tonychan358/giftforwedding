8b8df9da46de5fc97b1da6e51f95d93cacadafee
tonychan358 <tonychan358@gmail.com>
2026-02-14 02:07:54 +0800

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,27 +1,71 @@
-// 婚禮當天讀取這個檔案，速度最快且最穩定
-// 類型(type): "audio" 或 "video"
-const WISHES_DATA = [
-    {
-        type: "audio",
-        name: "高中死黨 - 小美",
-        // 範例圖片與音效
-        cover: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?w=800", 
-        src: "https://actions.google.com/sounds/v1/cartoon/cartoon_boing.ogg", 
-        message: "親愛的！一定要幸福喔！看著妳穿婚紗真的好感動。"
+{
+        "type": "1",
+        "name": "audio",
+        "cover": "你美麗的姐Emily❤️",
+        "src": "https://drive.google.com/uc?export=download&id=1jUQR6EPCnibgBHiH8d6isDWX70cx-w9t",
+        "message": "https://drive.google.com/file/d/1OuBf9lr0HlfDyQFGF0E5Z0UroYTpLDUo/view?usp=drivesdk"
     },
     {
-        type: "video",
-        name: "表弟 - 阿豪",
-        // 範例影片縮圖與影片 (這裡用圖片代替影片作示範)
-        cover: "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?w=800",
-        src: "https://www.w3schools.com/html/mov_bbb.mp4", 
-        message: "這是我們全家錄給妳的影片！新婚快樂！"
+        "type": "2",
+        "name": "audio",
+        "cover": "Virginia Kwong",
+        "src": "https://drive.google.com/uc?export=download&id=18z5sr4J-a2_rtL7refdn-GSpaFnlqcBf",
+        "message": "https://drive.google.com/file/d/1TQeYE22sHLDu2SyW1J5y4z-5NHxusa1Z/view?usp=drivesdk"
     },
     {
-        type: "audio",
-        name: "同事 - Jason",
-        cover: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=800",
-        src: "https://actions.google.com/sounds/v1/cartoon/pop.ogg",
-        message: "恭喜恭喜！蜜月回來記得帶伴手禮 XD"
+        "type": "3",
+        "name": "audio",
+        "cover": "Tung Tung & Thomas",
+        "src": "https://drive.google.com/uc?export=download&id=1NMgXctS1m5EZ9k7UM49TpBJxWm68Y527",
+        "message": "https://drive.google.com/file/d/15g0E6-stwMhqvIiWKGEOfxiKkRUpz2ci/view?usp=drivesdk"
+    },
+    {
+        "type": "4",
+        "name": "audio",
+        "cover": "Sin ling",
+        "src": "https://drive.google.com/uc?export=download&id=1w3R-DNkBaHt1Olz1LsOORhds_MAP7lV2",
+        "message": "https://drive.google.com/file/d/1O_HeV_iSlscUuvmkg69pas1Pn_n43AM4/view?usp=drivesdk"
+    },
+    {
+        "type": "5",
+        "name": "audio",
+        "cover": "Miss Wing",
+        "src": "https://drive.google.com/uc?export=download&id=1VaTr3QHqJ0R9XGi5CukO9ke-jKAe09wM",
+        "message": "https://drive.google.com/file/d/13pwIo39IFhMfR2cJtnTBNqsYH0wU55KI/view?usp=drivesdk"
+    },
+    {
+        "type": "6",
+        "name": "audio",
+        "cover": "善悠 善潼",
+        "src": "https://drive.google.com/uc?export=download&id=165bT6XxQwDBr_4QWsUZ8biDwXz2z7tEM",
+        "message": "https://drive.google.com/file/d/1zLFZ6fBOv_EOGI8E_vpEkfnIVLpM6bVt/view?usp=drivesdk"
+    },
+    {
+        "type": "7",
+        "name": "audio",
+        "cover": "心善",
+        "src": "https://drive.google.com/uc?export=download&id=1QJCSKQcTfLu68CP9h6_Zim7P4pcUUdRB",
+        "message": "https://drive.google.com/file/d/16-9MPiXM2eAtbooU2R6gXeqzCTvuIQjU/view?usp=drivesdk"
+    },
+    {
+        "type": "25",
+        "name": "video",
+        "cover": "穎塱",
+        "src": "https://drive.google.com/uc?export=download&id=1BN1NMm4Af90VMQAQAhFvEZFnPD5-xN92",
+        "message": "https://drive.google.com/file/d/1wJT58htaMtgvBfR4w3bXA7yEqZbsjouK/view?usp=drive_link"
+    },
+    {
+        "type": "27",
+        "name": "video",
+        "cover": "戩灝",
+        "src": "https://drive.google.com/uc?export=download&id=11yUacSsfAFll3WBBY32sApzuvlZmxah3",
+        "message": "https://drive.google.com/file/d/1EKGSs_JCSTr59RN2lZGC99j-S0gyCygn/view?usp=drive_link"
+    },
+    {
+        "type": "28",
+        "name": "video",
+        "cover": "梁証博",
+        "src": "https://drive.google.com/uc?export=download&id=1TpQWuNLM8SIo9Lcv5drRmiKSAtVGUDTz",
+        "message": "https://drive.google.com/file/d/1kfJcjBlu1Y8OndLMzYSti7S3aUYorntm/view?usp=drive_link"
     }
 ];
